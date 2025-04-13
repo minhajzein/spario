63089263d72b25da93d26d8a373f31b80b209ae5
minhajgrohance <minhaj.grohance@gmail.com>
2025-04-13 22:07:39 +0530

    dashboard added

--- src/components/admin/AdminDashboard.jsx
+++ src/components/admin/AdminDashboard.jsx
@@ -16,71 +16,84 @@ function AdminDashboard() {
 			<div className='flex flex-col w-full gap-3'>
 				<div className='grid md:grid-cols-5 gap-3'>
 					<div className='bg-white gap-2 flex rounded-lg p-4 md:flex-col'>
-						<div className='bg-[#9897D9] mt-6 flex rounded-full size-12'>
+						<div className='bg-[#9897D9] md:md:mt-6 flex rounded-full size-14 md:size-12'>
 							<img
 								className='size-4 m-auto'
 								src='/images/outstanding.png'
 								alt=''
 							/>
 						</div>
-						<h3 className='text-ellipsis whitespace-nowrap overflow-hidden text-xs'>
-							Total Outstanding
-						</h3>
-						<h1 className='font-bold text-xl text-primary'>
-							{dashboard?.totals?.debit}
-						</h1>
+						<div className='flex flex-col md:gap-2'>
+							<h3 className='text-ellipsis whitespace-nowrap overflow-hidden md:text-xs'>
+								Total Outstanding
+							</h3>
+							<h1 className='font-bold text-2xl md:text-xl text-primary'>
+								{dashboard?.totals?.debit}
+							</h1>
+						</div>
 					</div>
 					<div className='bg-white gap-2 flex rounded-lg p-4 md:flex-col'>
-						<div className='bg-[#00900E]/30 mt-6 flex rounded-full size-12'>
+						<div className='bg-[#00900E]/30 md:md:mt-6 flex rounded-full size-14 md:size-12'>
 							<img className='size-4 m-auto' src='/images/paid.png' alt='' />
 						</div>
-						<h3 className='text-ellipsis whitespace-nowrap overflow-hidden text-xs'>
-							Total Paid
-						</h3>
-						<h1 className='font-bold text-xl text-pr-green'>
-							{dashboard?.totals?.credit}
-						</h1>
-					</div>
-					<div className='bg-white gap-2 flex rounded-lg p-4 md:flex-col'>
-						<div className='bg-[#EF6969] mt-6 flex rounded-full size-12'>
-							<img className='size-4 m-auto' src='/images/paid.png' alt='' />
+						<div className='flex flex-col md:gap-2'>
+							<h3 className='text-ellipsis whitespace-nowrap overflow-hidden md:text-xs'>
+								Total Paid
+							</h3>
+							<h1 className='font-bold text-2xl md:text-xl text-pr-green'>
+								{dashboard?.totals?.credit}
+							</h1>
 						</div>
-						<h3 className='text-ellipsis whitespace-nowrap overflow-hidden text-xs'>
-							Total Due
-						</h3>
-						<h1 className='font-bold text-xl text-pr-red'>
-							{dashboard?.totals?.debit - dashboard?.totals?.credit}
-						</h1>
 					</div>
 					<div className='bg-white gap-2 flex rounded-lg p-4 md:flex-col'>
-						<div className='bg-[#D2D1FF] mt-6 flex rounded-full size-12'>
+						<div className='bg-[#EF6969] md:md:mt-6 flex rounded-full size-14 md:size-12'>
+							<img className='size-4 m-auto' src='/images/due.png' alt='' />
+						</div>
+						<div className='flex flex-col md:gap-2'>
+							<h3 className='text-ellipsis whitespace-nowrap overflow-hidden md:text-xs'>
+								Total Due
+							</h3>
+
+							<h1 className='font-bold text-2xl md:text-xl text-pr-red'>
+								{dashboard?.totals?.debit - dashboard?.totals?.credit}
+							</h1>
+						</div>
+					</div>
+					<div className='bg-white gap-2 flex rounded-lg p-4 md:flex-col'>
+						<div className='bg-[#D2D1FF] md:md:mt-6 flex rounded-full size-14 md:size-12'>
 							<img
 								className='size-4 m-auto'
 								src='/images/active-store.png'
 								alt=''
 							/>
 						</div>
-						<h3 className='text-ellipsis whitespace-nowrap overflow-hidden text-xs'>
-							Total Stores
-						</h3>
-						<h1 className='font-bold text-xl text-primary'>
-							{dashboard?.totalStores}
-						</h1>
+						<div className='flex flex-col md:gap-2'>
+							<h3 className='text-ellipsis whitespace-nowrap overflow-hidden md:text-xs'>
+								Total Stores
+							</h3>
+
+							<h1 className='font-bold text-2xl md:text-xl text-primary'>
+								{dashboard?.totalStores}
+							</h1>
+						</div>
 					</div>
 					<div className='bg-white gap-2 flex rounded-lg p-4 md:flex-col'>
-						<div className='bg-[#E1B3F1] mt-6 flex rounded-full size-12'>
+						<div className='bg-[#E1B3F1] md:md:mt-6 flex rounded-full size-14 md:size-12'>
 							<img
 								className='size-4 m-auto'
-								src='/images/active-store.png'
+								src='/images/active-executives.png'
 								alt=''
 							/>
 						</div>
-						<h3 className='text-ellipsis whitespace-nowrap overflow-hidden text-xs'>
-							Total Executives
-						</h3>
-						<h1 className='font-bold text-xl text-primary'>
-							{dashboard?.totalExecutives}
-						</h1>
+						<div className='flex flex-col md:gap-2'>
+							<h3 className='text-ellipsis whitespace-nowrap overflow-hidden md:text-xs'>
+								Total Executives
+							</h3>
+
+							<h1 className='font-bold text-2xl md:text-xl text-primary'>
+								{dashboard?.totalExecutives}
+							</h1>
+						</div>
 					</div>
 				</div>
 			</div>
--- src/components/executive/Dashboard.jsx
+++ src/components/executive/Dashboard.jsx
@@ -9,7 +9,6 @@ function Dashboard() {
 		isLoading,
 		isSuccess,
 	} = useGetExecutiveDashboardDataQuery(executiveId)
-	console.log(dashboard)
 
 	return isLoading ? (
 		<Loading />
@@ -18,56 +17,66 @@ function Dashboard() {
 			<div className='flex flex-col gap-3'>
 				<div className='grid md:grid-cols-4 gap-3'>
 					<div className='bg-white gap-2 flex rounded-lg p-4 md:flex-col'>
-						<div className='bg-[#9897D9] mt-6 flex rounded-full size-12'>
+						<div className='bg-[#9897D9] md:md:mt-6 flex rounded-full size-14 md:size-12'>
 							<img
 								className='size-4 m-auto'
 								src='/images/outstanding.png'
 								alt=''
 							/>
 						</div>
-						<h3 className='text-ellipsis whitespace-nowrap overflow-hidden text-xs'>
-							Total Outstanding
-						</h3>
-						<h1 className='font-bold text-xl text-primary'>
-							{dashboard?.totals?.debit}
-						</h1>
+						<div className='flex flex-col md:gap-2'>
+							<h3 className='text-ellipsis whitespace-nowrap overflow-hidden md:text-xs'>
+								Total Outstanding
+							</h3>
+							<h1 className='font-bold text-2xl md:text-xl text-primary'>
+								{dashboard?.totals?.debit}
+							</h1>
+						</div>
 					</div>
 					<div className='bg-white gap-2 flex rounded-lg p-4 md:flex-col'>
-						<div className='bg-[#00900E]/30 mt-6 flex rounded-full size-12'>
+						<div className='bg-[#00900E]/30 md:md:mt-6 flex rounded-full size-14 md:size-12'>
 							<img className='size-4 m-auto' src='/images/paid.png' alt='' />
 						</div>
-						<h3 className='text-ellipsis whitespace-nowrap overflow-hidden text-xs'>
-							Total Paid
-						</h3>
-						<h1 className='font-bold text-xl text-pr-green'>
-							{dashboard?.totals?.credit}
-						</h1>
-					</div>
-					<div className='bg-white gap-2 flex rounded-lg p-4 md:flex-col'>
-						<div className='bg-[#EF6969] mt-6 flex rounded-full size-12'>
-							<img className='size-4 m-auto' src='/images/paid.png' alt='' />
+						<div className='flex flex-col md:gap-2'>
+							<h3 className='text-ellipsis whitespace-nowrap overflow-hidden md:text-xs'>
+								Total Paid
+							</h3>
+							<h1 className='font-bold text-2xl md:text-xl text-pr-green'>
+								{dashboard?.totals?.credit}
+							</h1>
 						</div>
-						<h3 className='text-ellipsis whitespace-nowrap overflow-hidden text-xs'>
-							Total Due
-						</h3>
-						<h1 className='font-bold text-xl text-pr-red'>
-							{dashboard?.totals?.debit - dashboard?.totals?.credit}
-						</h1>
 					</div>
 					<div className='bg-white gap-2 flex rounded-lg p-4 md:flex-col'>
-						<div className='bg-[#D2D1FF] mt-6 flex rounded-full size-12'>
+						<div className='bg-[#EF6969] md:md:mt-6 flex rounded-full size-14 md:size-12'>
+							<img className='size-4 m-auto' src='/images/due.png' alt='' />
+						</div>
+						<div className='flex flex-col md:gap-2'>
+							<h3 className='text-ellipsis whitespace-nowrap overflow-hidden md:text-xs'>
+								Total Due
+							</h3>
+
+							<h1 className='font-bold text-2xl md:text-xl text-pr-red'>
+								{dashboard?.totals?.debit - dashboard?.totals?.credit}
+							</h1>
+						</div>
+					</div>
+					<div className='bg-white gap-2 flex rounded-lg p-4 md:flex-col'>
+						<div className='bg-[#D2D1FF] md:md:mt-6 flex rounded-full size-14 md:size-12'>
 							<img
 								className='size-4 m-auto'
 								src='/images/active-store.png'
 								alt=''
 							/>
 						</div>
-						<h3 className='text-ellipsis whitespace-nowrap overflow-hidden text-xs'>
-							Total Stores
-						</h3>
-						<h1 className='font-bold text-xl text-primary'>
-							{dashboard?.totalStores}
-						</h1>
+						<div className='flex flex-col md:gap-2'>
+							<h3 className='text-ellipsis whitespace-nowrap overflow-hidden md:text-xs'>
+								Total Stores
+							</h3>
+
+							<h1 className='font-bold text-2xl md:text-xl text-primary'>
+								{dashboard?.totalStores}
+							</h1>
+						</div>
 					</div>
 				</div>
 			</div>
